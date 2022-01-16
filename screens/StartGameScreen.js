9a8b4b087e325926dc32c718a8ac92ac7ba0280a
minchun_nguyen <minchun_nguyen@yahoo.com>
2022-01-17 01:15:24 +0700

    add layout for game screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 const StartGameScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text> The  Game Screen</Text>
+      <Text style={styles.title}> The Game Screen</Text>
+      <View style={styles.inputContainer}>
+        <Text> Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -13,6 +21,14 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     alignItems: "center",
+  },
+  title: { fontSize: 20, marginVertical: 10 },
+  inputContainer: { width: 300, maxWidth: "80%", alignItems: "center" },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
   },
 });
 export default StartGameScreen;
